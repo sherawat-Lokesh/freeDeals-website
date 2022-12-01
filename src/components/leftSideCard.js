f29cf81a478218546ac9b8db45b0b48e1cfbe872
sherawat-Lokesh <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-12-01 11:55:48 +0530

    pagination completed and now working on deployment

--- src/components/leftSideCard.js
+++ src/components/leftSideCard.js
@@ -7,45 +7,39 @@ import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 
 
 
+
+
 const LeftCard = ({ cardData: cardComponent, getFileName }) => {
   //transition type
   const fadeUp = "fade-up";
 
   let [pageNo,setPageNo]=useState(10)
-  let emptyArr=[]
+  let [empArr,setEmptyArr]=useState(undefined)
 
 useEffect(() => {aos.init({ duration: 1000 / 2 })}, []);        
   
-pageNo<10?setPageNo(10):''
-pageNo===10 ? pageChange(0):null
+if(pageNo<10)setPageNo(10)
+if(pageNo===10) pageChange(0)
 
 function pageChange(num){
-
-if(cardComponent !==undefined) {
-  const mainArray=cardComponent.sort((a,b)=>b.id-a.id).map(val=>{return val});
-  const arrayLength=cardComponent.length;
-    if(pageNo===10 && num===-10)return;
-        if(arrayLength<pageNo && num ===10)return;
-       if(num !==0) setPageNo( pageNo+=num)
-              emptyArr=[]
-                for(let i=pageNo-10;i<=pageNo;i++){
-                    if(mainArray[i]!==undefined){
-                        emptyArr.push(mainArray[i])    
-             
-        }
-      }
-
-      console.log(emptyArr)
-      
-    }
+  if(cardComponent !==undefined) {
+      const arrayLength=cardComponent.length;
+        if(pageNo===10 && num===-10)return;
+          if(arrayLength<pageNo && num ===10)return;
+            if(num !==0) setPageNo( pageNo+=num)
+  }
 }
+//This is working only because pageChange function run before it and page logic is handled their
+const result= cardComponent.sort((a,b)=>b.id-a.id).slice(pageNo-10,pageNo)
+//It only works when the pageNo change if we try to re-render based on ``result`` the it get's in error so pageNo is neccessary here
+// eslint-disable-next-line
+useEffect(()=>{setEmptyArr(result)},[pageNo])
 
 
-
-if(emptyArr ===[] )return
+if(empArr ===undefined )return
   return (
     <div>
-      {emptyArr.sort((a, b) => (a.id - b.id))
+      {empArr.sort((a, b) => (a.id - b.id))
           .map((element, i) => {
             return (
               <div key={i} data-aos={fadeUp}className="cards mt3 ba  b--light-gray near-black { color: #111111 }bg-white { background-color: #FFFFFF } pv2  mr4 ml5  shadow-4 ">
@@ -70,9 +64,9 @@ if(emptyArr ===[] )return
         })
         .reverse()}
        
-        <div className=" bg-black ph4 pv3 center shadow-5 mv5" style={{marginRight:'27rem',marginLeft:'4rem'}}>
+        <div className=" bg-black ph4 pv1 center shadow-5 mv5  " style={{marginLeft:'4rem',marginRight:'2rem'}} >
         <div className='ml5'>
-            <button className='ml7' onClick={()=>{pageChange(-10);}}><ArrowBackIosSharpIcon /></button>
+            <button className='ml7'  onClick={()=>{pageChange(-10);}}><ArrowBackIosSharpIcon /></button>
             <button className='ml6'  onClick={()=>{pageChange(10);}}><ArrowForwardIosSharpIcon /></button>
         </div>
     </div>
